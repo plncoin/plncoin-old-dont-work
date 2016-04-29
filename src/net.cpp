ce00cba53777f7fc30ec5163dbef298616afa2d1
plncoin <paatrickk@gmail.com>
2016-04-29 19:45:49 +0200

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1192,13 +1192,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"plncoin.org", "plncoin.org"},
-    {"pool.plncoin.org", "pool.plncoin.org"}
+    {"plncoin.org", "seed.plncoin.org"},
+    {"plncoin.org", "pool.plncoin.org"}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"plncoin.org", "plncoin.org"},
-    {"pool.plncoin.org", "pool.plncoin.org"}
+    {NULL, NULL}
 };
 
 void ThreadDNSAddressSeed()
